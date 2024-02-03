c290a5c545e996af20ced1fb3957a1d385d6a318
Dipikahalunde <127616517+halunde@users.noreply.github.com>
2024-02-03 15:21:03 +0000

    datatypes

--- practice.js
+++ practice.js
@@ -6,4 +6,44 @@ let age=21;
 let salary=1000000;
 
 console.table({id,name,department,age,salary});
+//datatypes
 
+console.log(typeof("name"));
+console.log(typeof(10));
+console.log(typeof(1.2));
+console.log(typeof('a'));
+console.log(typeof(null));
+console.log(typeof(undefined));
+let a=[1,2,3,4,5];
+let val={
+     id:67,
+ name:"Dipika",
+ department:"IT",
+ age:21,
+ salary:1000000
+    
+};
+console.log(typeof(val));
+
+let value=true;
+console.log(typeof(value));
+let score=Number(value);
+console.log(typeof(score));
+console.log(score);
+
+
+
+let value2=3;
+
+let negvalue=-value2
+console.log(negvalue);
+console.log(value2);
+
+let prevalue1=2;
+let post=prevalue1++;
+console.log(post);
+console.log(prevalue1)
+
+let prevalue2=2;
+let pre=++prevalue2;
+console.log(pre)
